231b75cf51e196d76ea8048b0dc80bf53e24511f
Carlos <carlosdchaconmendez@gmail.com>
2025-01-05 15:54:18 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -62,10 +62,9 @@ function App() {
             height="250"
             src="https://www.youtube.com/embed/bZLojmRvRd8?si=jRfyvIQjk3rYQF2R"
             title="YouTube video player"
-            frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
+            // referrerpolicy="strict-origin-when-cross-origin"
+            // allowfullscreen
           ></iframe>
         </>
       ) : (
